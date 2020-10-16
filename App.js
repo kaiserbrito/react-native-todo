293b92cde1d84f88a7395acd02cd04be9d226186
Victor Guerra <victorbcguerra@gmail.com>
2020-10-16 16:28:16 +0100

    Refactored to be a todo app

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert
+} from 'react-native';
 
-export default function App() {
+import Header from './components/Header';
+import ListTodo from './components/ListTodo';
+import AddTodo from './components/AddTodo';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: Math.random().toString(),
+      todo: 'Buy food'
+    },
+    {
+      id: Math.random().toString(),
+      todo: 'Drink coffee'
+    },
+    {
+      id: Math.random().toString(),
+      todo: 'Play guitar'
+    },
+  ]);
+
+  const submitTodo = (todo) => {
+    if (todo) {
+      setTodos(prevTodos => ([{ id: Math.random().toString(), todo }, ...prevTodos]));
+    } else {
+      Alert.alert('Error', 'Please add a valid remider!', [
+        { text: 'Ok', style: 'cancel' }
+      ]);
+    }
+  }
+
+  const checkedTodo = (id) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss(); }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitTodo={submitTodo} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <ListTodo
+                  item={item}
+                  checkedTodo={checkedTodo}
+                />
+              )}
+            />
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 40
+  }
 });
+
+export default App;
